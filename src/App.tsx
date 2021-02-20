c469ffeec61076c138d125e54a8a9f50c8142f0a
mrx <1320504268@qq.com>
2021-02-20 22:54:11 +0800

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,30 @@
 import React from 'react';
-import styled from "styled-components";
-import {Switch, Route, Redirect, HashRouter as Router, Link} from 'react-router-dom';
+import {Switch, Route, Redirect, HashRouter as Router} from 'react-router-dom';
 import NoMatch from "./views/NoMatch";
-
-import x from './icons/jizhang.svg';
-
-console.log(x)
-
-const AppWrapper = styled.div`
-  color: #333;
-`
-
-const NavWrapper = styled.nav`
-  line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
-  > ul {
-    display:flex;
-    > li{
-      width: 33.3333%;
-      text-align:center;
-      display: flex;
-      flex-direction: column;
-      padding: 4px 0;
-      justify-content: center;
-      align-items: center;
-      .icon {
-        width: 24px;      
-        height: 24px;
-      }
-    }
-  }
-`;
+import Money from "./views/Money/index";
+import Statistics from "./views/Statistics/index";
+import Tags from "./views/Tags/Tags";
 
 function App() {
   return (
-    <AppWrapper>
-        <Router>
-            <Switch>
-                <Route path="/tags">
-                    <Tags />
-                </Route>
-                <Route path="/money">
-                    <Money />
-                </Route>
-                <Route path="/statistics">
-                    <Statistics />
-                </Route>
-                <Redirect exact from="/" to="/money" />
-                <Route path="/404">
-                    <NoMatch />
-                </Route>
-            </Switch>
-            <NavWrapper>
-                <ul>
-                    <li>
-                        <Link to="/tags">标签页</Link>
-                    </li>
-                    <li>
-                        <Link to="/money">记账页</Link>
-                    </li>
-                    <li>
-                        <Link to="/statistics">统计页</Link>
-                    </li>
-                </ul>
-            </NavWrapper>
-        </Router>
-    </AppWrapper>
+      <Router>
+          <Switch>
+              <Route path="/tags">
+                  <Tags />
+              </Route>
+              <Route path="/money">
+                  <Money />
+              </Route>
+              <Route path="/statistics">
+                  <Statistics />
+              </Route>
+              <Redirect exact from="/" to="/money" />
+              <Route path="/404">
+                  <NoMatch />
+              </Route>
+          </Switch>
+      </Router>
   );
-}
-
-function Tags() {
-    return <div>tags</div>
-}
-
-function Money() {
-    return <div>money</div>
-}
-
-function Statistics() {
-    return <div>statistics</div>
 }
 
 export default App;
---
+++ src/views/Statistics/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '../../components/Layout';
+import React from 'react';
+
+function Index() {
+    return (
+        <Layout>
+            <h2>统计</h2>
+        </Layout>
+    );
+}
+
+export default Index;

1e906cf84c0d18baf3860facad4407b1a838d833
mrx <1320504268@qq.com>
2021-02-22 22:35:10 +0800

    tag selector

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from "react";
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   background: #FFFFFF; padding: 12px 16px;
   flex-grow: 1; display:flex; flex-direction: column;
   justify-content: flex-end; align-items: flex-start;
@@ -9,6 +10,10 @@ const TagsSection = styled.section`
        background: #D9D9D9; border-radius: 18px;
        display:inline-block; padding: 3px 18px; 
        font-size: 14px; margin: 8px 12px;
+       cursor: pointer;
+       &.selected {
+        background: #f60;
+       }
     }
   }
   > button{
@@ -17,5 +22,48 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `;
+
+const TagsSection: React.FC = () => {
+  const [tags, setTags] = useState<string[]>(['衣', '食物', '住', '行'])
+
+  const [selectedTags, setSelectedTags] = useState<string[]>([])
+
+  const addTag = (): void => {
+    const tagName = window.prompt('请输入新标签的名称')
+    if (tagName !== null) {
+      if (tags.indexOf(tagName) >= 0) {
+        alert('标签名已存在')
+        return
+      }
+      setTags([...tags, tagName])
+
+    }
+  }
+
+  const toggleTags = (tag: string): void => {
+    if (selectedTags.indexOf(tag) >= 0) {
+      setSelectedTags(selectedTags.filter(t => t !== tag))
+    } else {
+      setSelectedTags([...selectedTags, tag])
+    }
+  }
+
+  const getActiveClass = (tag: string): string => {
+    return selectedTags.indexOf(tag) >= 0 ? 'selected' : ''
+  }
+
+  return (
+    <Wrapper>
+      <ol>
+        {
+          tags.map(tag => {
+            return <li key={tag} className={getActiveClass(tag)} onClick={() => toggleTags(tag)}>{tag}</li>
+          })
+        }
+      </ol>
+      <button onClick={addTag}>新增标签</button>
+    </Wrapper>
+  )
+}
 
 export {TagsSection};
